413d9aa9d1df90365c8da2526303842160c48b59
ElianaCariac <103609851+ElianaCariac@users.noreply.github.com>
2023-03-08 10:36:41 -0300

    Add files via upload

---
+++ js/eventos.js
@@ -0,0 +1,71 @@
+function enRosa1() {
+Element =document.getElementById("sobremi");
+Element.src="img/sobremi2.png";
+}
+function enNegro1() {
+    Element =document.getElementById("sobremi");
+    Element.src="img/sobremi.png";
+}
+
+function enRosa2() {
+    Element =document.getElementById("experiencia");
+    Element.src="img/experiencia2.png";
+}
+ function enNegro2() {
+    Element =document.getElementById("experiencia");
+    Element.src="img/experiencia.png";
+}
+
+function enRosa3() {
+    Element =document.getElementById("habilidades");
+    Element.src="img/habilidades2.png";
+}
+function enNegro3() {
+    Element =document.getElementById("habilidades");
+    Element.src="img/habilidades.png";
+}
+
+function enRosa4() {
+    Element =document.getElementById("estudios");
+    Element.src="img/estudios2.png";
+}
+function enNegro4() {
+    Element =document.getElementById("estudios");
+    Element.src="img/estudios.png";
+}
+
+function enRosa5() {
+    Element =document.getElementById("be");
+    Element.src="img/behance2.png";
+}
+function enNegro5() {
+    Element =document.getElementById("be");
+    Element.src="img/behance.png";
+}
+
+function enRosa6() {
+    Element =document.getElementById("gi");
+    Element.src="img/github2.png";
+}
+function enNegro6() {
+    Element =document.getElementById("gi");
+    Element.src="img/github.png";
+}
+
+function enRosa7() {
+    Element =document.getElementById("li");
+    Element.src="img/linkedin2.png";
+}
+function enNegro7() {
+    Element =document.getElementById("li");
+    Element.src="img/linkedin.png";
+}
+
+function enRosa8() {
+    Element =document.getElementById("you");
+    Element.src="img/youtube2.png";
+}
+function enNegro8() {
+    Element =document.getElementById("you");
+    Element.src="img/youtube.png";
+}
